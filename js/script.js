2ff5d306b416aae9cf98813bf2cf1149ba6cd8f7
AdiChops <44070813+AdiChops@users.noreply.github.com>
2019-01-16 13:15:49 -0500

    Added "Enter" functionality

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ addEventListener('load', function () {
     loadFavourites();
 });
 document.getElementById("searchbtn").addEventListener('click', function () {
-    if (document.getElementById("search").value != '' && document.getElementById("search").value != ',' && document.getElementById("search").value != ' ') {
-        document.getElementById("error").textContent = "";
         performSearch();
-    } else {
-        document.getElementById("error").textContent = "Please enter a valid query";
-    }
 });
-document.getElementById("search").addEventListener('keyup', function () {
-    document.getElementById("results").innerHTML = "";
+document.getElementById("search").addEventListener('keyup', function (e) {
+    if (e.keyCode == 13) // if "Enter" was hit
+        performSearch();
+    else
+        document.getElementById("results").innerHTML = "";
 });
 document.getElementById("results").addEventListener('click', function (e) {
     let toFavourite = e.target;
@@ -42,60 +40,66 @@ document.getElementById("items").addEventListener('click', function (e) {
     }
 })
 let performSearch = function () {
-    let hasResults = false;
-    let result = "<h1>Search Results</h1>"; // Render header to explain what div contains
-    for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'count') {
-            let currentObj = JSON.parse(decodeURIComponent(localStorage.getItem(localStorage.key(i))));
-            if (currentObj["keywords"].includes(document.getElementById("search").value)) {
-                // Render body as HTML code
-                hasResults = true;
-                let body = document.createElement('div');
-                body.innerHTML = currentObj['body'];
-                result += `<div>
+    if (document.getElementById("search").value != '' && document.getElementById("search").value != ',' && document.getElementById("search").value != ' ') {
+        document.getElementById("error").textContent = "";
+        let hasResults = false;
+        let result = "<h1>Search Results</h1>"; // Render header to explain what div contains
+        for (let i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i) !== 'count') {
+                let currentObj = JSON.parse(decodeURIComponent(localStorage.getItem(localStorage.key(i))));
+                if (currentObj["keywords"].includes(document.getElementById("search").value)) {
+                    // Render body as HTML code
+                    hasResults = true;
+                    let body = document.createElement('div');
+                    body.innerHTML = currentObj['body'];
+                    result += `<div>
                         <button class ="favourited" name=${localStorage.key(i)} value="${localStorage.getItem(localStorage.key(i))}"><i class="fas fa-star"></i></button>
                         <p>${currentObj['title']}</p>
                         <div class="inner">${body.textContent}</div>
                     </div>`;
+                }
             }
         }
-    }
-    // perform API call
-    fetch("https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000").then(function (resp) {
-        return resp.json();
-    }).then(function (info) {
-        let count = 0;
-        if (localStorage.count != undefined)
-            count = localStorage.count;
-        // if count variable is missing but favourites exist
-        else if (localStorage.count == undefined && localStorage.length > 0) {
-            //get the number of the last favourited item to reset count at that place
-            let lastFave = localStorage.key(localStorage.length - 1);
-            // favourite has 9 letters so last favourite number starts at 9th index
-            count = lastFave.substring(9);
-        }
+        // perform API call
+        fetch("https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000").then(function (resp) {
+            return resp.json();
+        }).then(function (info) {
+            let count = 0;
+            if (localStorage.count != undefined)
+                count = localStorage.count;
+            // if count variable is missing but favourites exist
+            else if (localStorage.count == undefined && localStorage.length > 0) {
+                //get the number of the last favourited item to reset count at that place
+                let lastFave = localStorage.key(localStorage.length - 1);
+                // favourite has 9 letters so last favourite number starts at 9th index
+                count = lastFave.substring(9);
+            }
 
-        for (let i of info) {
-            if (i['keywords'].includes(document.getElementById("search").value) && !searchFavourites(i)) {
-                hasResults = true;
-                count++;
-                // Render body as HTML code
-                let body = document.createElement('div');
-                body.innerHTML = i['body'];
-                // Using name instead of id below to prevent duplicate ids
-                result += `<div>
+            for (let i of info) {
+                if (i['keywords'].includes(document.getElementById("search").value) && !searchFavourites(i)) {
+                    hasResults = true;
+                    count++;
+                    // Render body as HTML code
+                    let body = document.createElement('div');
+                    body.innerHTML = i['body'];
+                    // Using name instead of id below to prevent duplicate ids
+                    result += `<div>
                     <button class="unfavourited" value=${encodeURIComponent(JSON.stringify(i))} name="favourite${count}"><i class="fas fa-star"></i></button>
                     <p>${i['title']}</p>
                     <div class="inner">${body.textContent}</div>
                 </div>`;
-                localStorage.count = count;
+                    localStorage.count = count;
+                }
             }
-        }
-        document.getElementById("results").innerHTML = result;
-        if (!hasResults) {
-            document.getElementById("results").innerHTML += "<h3>No results found</h3>"
-        }
-    })
+            document.getElementById("results").innerHTML = result;
+            if (!hasResults) {
+                document.getElementById("results").innerHTML += "<h3>No results found</h3>"
+            }
+        })
+    } // end-if valid query
+    else {
+        document.getElementById("error").textContent = "Please enter a valid query";
+    }//if invalid query
 }
 // This function verifies if a result returned by the API call already exists in the favourites
 let searchFavourites = function (result) {
